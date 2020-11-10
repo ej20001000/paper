050439e7f174054b91d318904b2b87ed5a0b08ad
ej20001000 <ejayyj1225@gmail.com>
2020-11-10 23:12:14 +0900

    typeChange, range, some quiz

--- python/practice.py
+++ python/practice.py
@@ -318,3 +318,54 @@
 
 # python.remove('김태호')
 # print(python)
+
+# # 자료 구조의 변경
+# # 커피숍
+# menu = {'커피', '우유', '주스'}
+# print(menu , type(menu))
+
+# menu = list(menu)
+# print(menu , type(menu))
+
+# menu = tuple(menu)
+# print(menu , type(menu))
+
+# menu = set(menu)
+# print(menu , type(menu))
+
+# 퀴즈 4
+
+# idList = []
+
+# i = 1
+
+# while i <= 20:
+#     idList.append(i)
+#     i += 1
+
+# from random import *
+
+# # print(idList)
+# shuffle(idList)
+
+# winners = sample(idList, 4)
+# print(winners[0])
+# print(winners[1], winners[2], winners[3])
+
+# 해설
+
+# from random import *
+# users = range(1, 21) # 1부터 20까지 숫자를 생성
+# # print(type(users)) # range type
+# users = list(users)
+# # print(type(users)) # list type
+
+# # print(users)
+# shuffle(users)
+# # print(users)
+
+# winners = sample(users, 4)
+# print(' -- 담청자 발표 -- ')
+# print('치킨 담청자 : {0}'.format(winners[0]))
+# print('커피 담청자 : {0}'.format(winners[1:]))
+# print('축하합니다')
