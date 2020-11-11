7d359392602ddd84ba3917ef67e6b606b2f69b89
ej20001000 <ejayyj1225@gmail.com>
2020-11-11 21:53:17 +0900

    for & while

--- python/practice.py
+++ python/practice.py
@@ -369,3 +369,49 @@
 # print('치킨 담청자 : {0}'.format(winners[0]))
 # print('커피 담청자 : {0}'.format(winners[1:]))
 # print('축하합니다')
+
+# if
+
+# weather = input('오늘 날씨는 어때요?')
+
+# if weather == '비' or weather == '눈':
+#     print('우산을 챙기세요')
+# elif weather == '미세먼지':
+#     print('마스크를 챙기세요')
+# else:
+#     print('준비물 필요 없어요')
+
+# temp = int(input('기온은 어때요?'))
+# if 30 <= temp:
+#     print('너무 더워요, 나가지 마세요')
+# elif 10 <= temp and temp < 30:
+#     print('괜찮은 날씨예요')
+# elif 0 <= temp < 10:
+#     print('외투를 챙기세요')
+# else:
+#     print('너무 추워요, 나가지 마세요')
+
+# for waitingNo in range(1, 6):
+#     print('대기번호 : {0}'.format(waitingNo))
+
+# starbucks = ['아이언맨', '토르', '아이엠 그루트']
+# for customer in starbucks:
+#     print('{0}, 커피가 준비되었습니다'.format(customer))
+
+# while
+
+# customer = '토르'
+# index = 5
+# while index >= 1:
+#     print('{0}, 커피가 준비되었습니다. {1} 번 남았어요'.format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print('커피는 폐기처분되었습니다')
+
+# customer = '토르'
+# person = 'unknown'
+
+# while person != customer:
+#     print('{0}, 커피가 준비 되었습니다.'.format(customer))
+#     person = input('이름이 어떻게 되세요?')
+    
