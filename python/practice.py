b185c00c1253535291fc96b5c04a7c4615a96d44
ej20001000 <ejayyj1225@gmail.com>
2020-12-15 23:03:45 +0900

    quiz

--- python/practice.py
+++ python/practice.py
@@ -532,3 +532,29 @@
 # 퀴즈 #6
 # 표준 체중
 
+# def avgWeight():
+#     height = input('키?')
+#     height = int(height)
+#     gender = input('성별?\n1.남자\n2.여자')
+#     times = 0
+    
+#     if gender == '1':
+#         times = 22
+#     else:
+#         times = 21
+
+#     val=height*height*times/10000
+#     print(round(val,2))
+
+# avgWeight()
+
+# def stdWeight(height, gender): # 키 m 단위 (실수), 성별 "남자" / "여자"
+#     if gender == "남자":
+#         return height * height * 22
+#     else:
+#         return height * height * 21
+
+# height = 175
+# gender = "남자"
+# weight = round(stdWeight(height / 100, gender), 2)
+# print('키 {}cm {}의 표준 체중은 {}kg 입니다.'.format(height, gender, weight))
