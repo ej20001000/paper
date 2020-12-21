e752d097ee21cd7449f180838706583bbe2b718b
ej20001000 <ejayyj1225@gmail.com>
2020-12-21 23:35:06 +0900

    py

--- python/practice.py
+++ python/practice.py
@@ -583,3 +583,60 @@
 # print('입력하신 값은 '+answer+' 입니다.')
 
 # input은 문자열로 변수에 저장
+
+# # 빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
+# print('{0: >10}'.format(500))
+# # 양수일 땐 +로 표시, 음수일 땐 -로 표시
+# print('{0: >+10}'.format(500))
+# print('{0: >+10}'.format(-500))
+# # 왼쪽 정렬하고, 빈칸으로 _로 채움
+# print('{0:_<10}'.format(500))
+# # 3자리 마다 콤마를 찍어주기
+# print('{0:,}'.format(10000000000))
+# # 3자리 마다 콤마를 찍어주기, +- 부호도 붙이기
+# print('{0:+,}'.format(10000000000))
+# # 3자리 마다 콤마를 찍어주기, 부호도 붙이고, 자릿수 확보하기
+# # 돈이 많으면 행복하니까 빈 자리는 ^ 로 채워주기
+# print('{0:^<+30,}'.format(1000000000000))
+# # 소수점 출력
+# print('{0:f}'.format(5/3))
+# # 소수점 특정 자리수 까지만 표시 (소수점 3째 자리에서 반올림)
+# print('{0:.2f}'.format(5/3))
+
+# 파일 입출력
+# scoreFile = open('score.txt', 'w', encoding='utf8')
+# print('수학 : 0', file=scoreFile)
+# print('영어 : 50', file=scoreFile)
+# scoreFile.close()
+
+# scoreFile=open('score.txt', 'a', encoding='utf8')
+# scoreFile.write ('과학 : 80')
+# scoreFile.write('\n코딩 : 100')
+# scoreFile.close()
+
+# scoreFile=open('score.txt','r',encoding='utf8')
+# print(scoreFile.read())
+# scoreFile.close()
+
+# scoreFile=open('score.txt','r',encoding='utf8')
+# print(scoreFile.readline()) # 줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
+# print(scoreFile.readline())
+# print(scoreFile.readline())
+# print(scoreFile.readline())
+
+# scoreFile.close()
+
+# scoreFile=open('score.txt','r',encoding='utf8')
+# while True:
+#     line=scoreFile.readline()
+#     if not line:
+#         break
+#     print(line)
+# scoreFile.close()
+
+# scoreFile=open('score.txt','r',encoding='utf8')
+# lines = scoreFile.readlines() # list 형태로 저장
+# for line in lines:
+#     print(line, end='')
+# scoreFile.close()
+
