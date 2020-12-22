652e850768f4d2ea619edb49ad820a7ae1fb5347
ej20001000 <ejayyj1225@gmail.com>
2020-12-22 19:22:15 +0900

    pickle(saving data type into a file)

--- python/practice.py
+++ python/practice.py
@@ -640,3 +640,16 @@
 #     print(line, end='')
 # scoreFile.close()
 
+# pickle
+# import pickle
+# profileFile=open('profile.pickle','wb')
+# profile={'이름':'박명수', '나이':30, '취미':['축구','골프','코딩']}
+# print(profile)
+# pickle.dump(profile, profileFile) # profile에 있는 정보를 file에 저장
+# profileFile.close()
+
+# import pickle
+# profileFile=open('profile.pickle','rb')
+# profile = pickle.load(profileFile) # file에 있는 정보를 profile에 불러오기
+# print(profile)
+# profileFile.close()
