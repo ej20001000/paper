3f30b5774f2539a2ea01a5c8df9d831fc2585528
ej20001000 <ejayyj1225@gmail.com>
2020-12-23 20:37:15 +0900

    python practice

--- python/practice.py
+++ python/practice.py
@@ -680,3 +680,73 @@
 # #         부서 : 
 # #         이름 :
 # #         업무 요약 :'''.format(i))
+
+# class
+# # 마린 : 공격 유닛, 군인. 총을 쏠 수 있음
+# name = '마린'
+# hp = 40
+# damage = 5
+
+# print('{} 유닛이 생성되었습니다.'.format(name))
+# print('체력 {}, 공격력 {}\n'.format(hp,damage))
+
+# # 탱크 : 공격 유닛, 탱크. 포를 쏠 수 있는데, 일반 모드 / 시즈 모드.
+# tankName = '탱크'
+# tankHp=150
+# tankDamage=35
+
+# print('{} 유닛이 생성되었습니다.'.format(tankName))
+# print('체력 {}, 공격력 {}\n'.format(tankHp,tankDamage))
+
+# def attack(name, location, damage):
+#     print('{} : {} 방향으로 적군을 공격 합니다. [공격력 {}]'.format(name, location, damage))
+
+# attack(name, '1시', damage)
+# attack(tankName, '1시', tankDamage)
+
+class Unit:
+    def __init__(self, name, hp, damage): # __init__ : 생성자
+        self.name=name
+        self.hp=hp
+        self.damage=damage
+        print('{} 유닛이 생성 되었습니다.'.format(self.name))
+        print('체력 {}, 공격력 {}'.format(self.hp, self.damage))
+
+class AttackUnit:
+    def __init__(self, name, hp, damage): # __init__ : 생성자
+        self.name=name
+        self.hp=hp
+        self.damage=damage
+
+    def attack(self, location):
+        print('{} : {} 방향으로 적군을 공격 합니다. [공격력 {}]'.format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print('{} : {} 데미지를 입었습니다.'.format(self.name, damage))
+        self.hp -= damage
+        print('{} : 현재 체력은 {} 입니다.'.format(self.name, self.hp))
+        if self.hp <= 0:
+            print('{} : 파괴되었습니다.'.format(self.name))
+
+# 파이어뱃 : 공격 유닛, 화염방사기.
+firebat1 = AttackUnit('파이어뱃', 50, 16)           
+firebat1.attack('5시')
+
+# 공격 2번 받는다고 가정
+firebat1.damaged(25)
+firebat1.damaged(25)
+
+# # marine1 = Unit('마린', 40, 5)
+# # marine2 = Unit('마린', 40, 5)
+# # tank = Unit('탱크', 150, 35)
+
+# # 레이스 : 공중 유닛, 비행기. 클로킹 (상대방에게 보이지 않음)
+# wraith1 = Unit('레이스', 80, 5)
+# print('유닛 이름 : {}, 공격력 : {}'.format(wraith1.name, wraith1.damage))
+
+# # 마인드 컨트롤 : 상대방 유닛을 내것으로 만드는 것 (빼앗음)
+# wraith2 = Unit('빼앗은 레이스', 80, 5)
+# wraith2.clocking = True
+
+# if wraith2.clocking == True:
+#     print('{} 는 현재 클로킹 상태입니다.'.format(wraith2.name))
