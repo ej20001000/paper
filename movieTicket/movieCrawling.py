575ff4fbd09db95052b2ba26dd36e233d7e3dbf0
ej20001000 <ejayyj1225@gmail.com>
2020-12-29 20:17:42 +0900

    MovieCrawlingEnd

--- movieTicket/movieCrawling.py
+++ movieTicket/movieCrawling.py
@@ -18,9 +18,9 @@ def jobFunction():
         imax = imax.find_parent('div', class_ ='col-times')
         title = imax.select_one('div.info-movie > a > strong').text.strip()
         bot.sendMessage(chat_id = 1491640870, text = title + ' IMAX 예매가 열렸습니다.')
-    else:
-        bot.sendMessage(chat_id = 1491640870, text = 'IMAX 예매가 아직 열리지 않았습니다.')
+        sched.pause()
 
 sched = BlockingScheduler()
 sched.add_job(jobFunction, 'interval', seconds = 30)
 sched.start()
+
