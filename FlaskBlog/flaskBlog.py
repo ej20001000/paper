3c7a8482f997f4ce55d44aa04929989d6112a385
ej20001000 <ejayyj1225@gmail.com>
2021-01-06 22:09:51 +0900

    flask register, login

--- FlaskBlog/flaskBlog.py
+++ FlaskBlog/flaskBlog.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0ace39f6f752fc4b0f8f05106b322ae0'
+
 rt = render_template
 posts = [
     {
@@ -24,6 +28,17 @@ def home():
 @app.route('/about')
 def about():
     return rt('about.html', title='about')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return rt('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return rt('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
