c0f4a52f4212abfcfa8be914643a8f91e90b728b
ej20001000 <ejayyj1225@gmail.com>
2021-04-28 23:36:11 +0900

    wack a mole complete

--- whack-a-mole/app.js
+++ whack-a-mole/app.js
@@ -4,8 +4,13 @@ const timeLeft = document.querySelector('#time-left')
 let score = document.querySelector('#score')
 
 let result = 0
+let currentTime = timeLeft.textContent
+let hitPosition
 
 function randomSquare() {
+    if(hit){
+        hit.style.backgroundImage = ""
+    }
     square.forEach(className => {
         className.classList.remove('mole')
     })
@@ -13,14 +18,18 @@ function randomSquare() {
     randomPosition.classList.add('mole')
 
     //assign the id of the randomPosition to hitPosition for us to use later
-    hitPosition = randomPostion.id
+    hitPosition = randomPosition.id
 }
 
+let hit
+
 square.forEach(id => {
-    id.addEventListener('mouseup', () => {
+    id.addEventListener('click', () => {
         if(id.id === hitPosition){
-            result == result + 1
+            result = result + 1
             score.textContent = result
+            id.style.backgroundImage = "url('red.png')"
+            hit = id
         }
     })
 })
@@ -31,3 +40,15 @@ function moveMole(){
 }
 
 moveMole()
+
+function countDown(){
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime === 0){
+        clearInterval(timerId)
+        alert('GAME OVER! Your final score is ' + result)
+    }
+}
+
+let timerId = setInterval(countDown, 1000)
