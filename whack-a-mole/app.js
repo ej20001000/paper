83ac0b9f83f52a696ba752a655634ad24ba9e8d2
ej20001000 <ejayyj1225@gmail.com>
2021-04-29 18:44:20 +0900

    whack a mole bug fix and improve

--- whack-a-mole/app.js
+++ whack-a-mole/app.js
@@ -1,15 +1,24 @@
 const square = document.querySelectorAll('.square')
 const mole = document.querySelectorAll('.mole')
 const timeLeft = document.querySelector('#time-left')
+const start = document.querySelector('.start')
+
 let score = document.querySelector('#score')
 
 let result = 0
 let currentTime = timeLeft.textContent
 let hitPosition
+let hit
+let alreadyHit
+let timeId = null
+let timerId = null
+
+start.addEventListener('click', startGame)
 
 function randomSquare() {
     if(hit){
         hit.style.backgroundImage = ""
+        alreadyHit = false
     }
     square.forEach(className => {
         className.classList.remove('mole')
@@ -21,34 +30,37 @@ function randomSquare() {
     hitPosition = randomPosition.id
 }
 
-let hit
-
-square.forEach(id => {
-    id.addEventListener('click', () => {
-        if(id.id === hitPosition){
-            result = result + 1
-            score.textContent = result
-            id.style.backgroundImage = "url('red.png')"
-            hit = id
-        }
-    })
-})
-
-function moveMole(){
-    let timerId = null
+function startGame(){
+    start.classList.remove('start')
+    start.removeEventListener('click', startGame)
+    
+    timerId = setInterval(countDown, 1000)
     timeId = setInterval(randomSquare, 1000)
+    square.forEach(id => {
+        id.addEventListener('click', () => {
+            if(id.id === hitPosition && !alreadyHit){
+                result = result + 1
+                score.textContent = result
+                id.style.backgroundImage = "url('x.png')"
+                hit = id
+                alreadyHit = true
+            }
+        })
+    })
 }
-
-moveMole()
 
 function countDown(){
     currentTime--
     timeLeft.textContent = currentTime
 
-    if(currentTime === 0){
+    if(currentTime < 0){
         clearInterval(timerId)
+        clearInterval(timeId)
         alert('GAME OVER! Your final score is ' + result)
+        start.classList.add('start')
+        start.addEventListener('click', startGame)
+        currentTime = 60
+        timeLeft.textContent = currentTime
+        document.querySelector('.mole').classList.remove('mole')
     }
 }
-
-let timerId = setInterval(countDown, 1000)
