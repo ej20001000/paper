930aa719d15b6ab438deec96c512054c48cf6c5e
ej20001000 <ejayyj1225@gmail.com>
2021-12-01 17:44:25 +0900

    a

--- codingTestPython/8/Baekjoon1193.py
+++ codingTestPython/8/Baekjoon1193.py
@@ -1,13 +1,27 @@
 a = int(input())
 x = 1
-y = 2
+y = 1
+i = 0
 
-if a == 1:
-    print('1/1')
-elif a == 2:
-    print('1/2')
-else:
-    for i in range(2, a):
-            
-
+while i < a - 1:
+    if x == 1 and y ==1 :
+        y += 1
+        i += 1
+    elif x == 1 and y % 2 != 0:
+        y += 1
+        i += 1
+    elif x == 1 and y % 2 == 0:
+        while y != 1 and i < a - 1:
+            x += 1
+            y -= 1
+            i += 1
+    elif y == 1 and x % 2 == 0:
+        x += 1
+        i += 1
+    elif y == 1 and x % 2 != 0:
+        while x != 1 and i < a -1:
+            x -= 1
+            y += 1
+            i += 1
     
+print('{}/{}'.format(x, y))
