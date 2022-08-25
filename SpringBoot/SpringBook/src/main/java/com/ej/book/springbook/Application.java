8977c2a36f719401cc56e985b7cd1175a5720c49
ej20001000 <ejayyj1225@gmail.com>
2022-08-25 17:06:52 +0900

    Test code 생성

---
+++ SpringBoot/SpringBook/src/main/java/com/ej/book/springbook/Application.java
@@ -0,0 +1,11 @@
+package com.ej.book.springbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
