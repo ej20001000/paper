9eca16955d05b7d507cd3b97cf93f93c90637a91
ej20001000 <ejayyj1225@gmail.com>
2022-11-29 01:01:25 +0900

    백준 문제풀이 10개

---
+++ javaPaper/src/javaAlgorithmStudy/Baekjoon1001.java
@@ -0,0 +1,14 @@
+package javaAlgorithmStudy;
+
+import java.util.Scanner;
+
+public class Baekjoon1001 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println(x-y);
+    }
+}
---
+++ javaPaper/src/javaAlgorithmStudy/Baekjoon2741.java
@@ -0,0 +1,15 @@
+package javaAlgorithmStudy;
+
+import java.util.Scanner;
+
+public class Baekjoon2741 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int x = in.nextInt();
+
+        for(int i = 1;i <= x;i++){
+            System.out.println(i);
+        }
+    }
+}
