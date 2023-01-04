d28675aee77377897ee8309e446d1ac5503b3829
ej20001000 <ejayyj1225@gmail.com>
2023-01-05 00:16:36 +0900

    solved 1934

--- PythonAlgorithm/basic/boj1934.py
+++ PythonAlgorithm/basic/boj1934.py
@@ -1,13 +1,11 @@
 # 최소공배수
 
-def cal(a, b):
-    A, B = a, b
-    while a % b != 0:
-        print('a, b=', a, b)
-        a, b = b, a % b
-    print('a, b=', a, b)
-    print('answer =', (A * B) // b)
-
 for _ in range(int(input())):
     a, b = map(int, input().split())
-    cal(a, b)
+
+    mult = a * b
+
+    while a % b != 0:
+        a, b = b, a % b
+    
+    print(int(mult / b))
