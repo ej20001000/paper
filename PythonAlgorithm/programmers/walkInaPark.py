b7f0270b786bcb855187ab1b658b7c35f21bf4e1
ej20001000 <ejayyj1225@gmail.com>
2023-08-10 17:04:08 +0900

    20230810 react js study

---
+++ PythonAlgorithm/programmers/walkInaPark.py
@@ -0,0 +1,12 @@
+def solution(park, routes):
+    for p in park:
+        
+        print(p.find("S"))
+    answer = []
+    return answer
+
+
+park=["SOO","OOO","OOO"]
+routes=	["E 2","S 2","W 1"]
+
+print(solution(park, routes))
