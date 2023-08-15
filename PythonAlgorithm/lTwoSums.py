4711d234b1d09b04c37546f9439f867b4cc3cbd4
ej20001000 <ejayyj1225@gmail.com>
2023-08-15 15:07:01 +0900

    twosums algorithm solved

---
+++ PythonAlgorithm/lTwoSums.py
@@ -0,0 +1,21 @@
+from typing import List;
+
+class Solution:
+    def twoSum(nums: List[int], target: int) -> List[int]:
+
+        checkIf = False
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    returnList = [i, j]
+                    checkIf = True
+                    break
+            if checkIf:
+                break
+
+        return returnList
+
+        
+    
+    twoSum([3,3], 6)
