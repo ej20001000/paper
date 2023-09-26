502e2c07cb4c21e14c89a7214016ff3ac8980fcf
ej20001000 <ejayyj1225@gmail.com>
2023-09-26 09:15:10 +0900

    Lecture docker

---
+++ Lecture/15_docker/docker-build-project/src/main/java/com/ohgiraffers/docker/DockerBuildProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBuildProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerBuildProjectApplication.class, args);
+    }
+
+}
